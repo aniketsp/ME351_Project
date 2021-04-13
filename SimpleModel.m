993d28e1622571023fb7496f5172ca3c68101c01
Cenzofy <35461425+Cenzofy@users.noreply.github.com>
2021-04-12 22:42:29 -0400

    Corrected Velocity

--- SimpleModel.m
+++ SimpleModel.m
@@ -1,13 +1,13 @@
-gravity = 9.81;     %m/s^2
-rho = 998;          %kg/m^3
-diameter = 0.00794;
-length = 0.3;
-area = (diameter/2)^2*pi
-height = 10;
+gravity = 9.81;             %m/s^2
+rho = 998;                  %kg/m^3
+diameter = 0.00794;         %m
+length = 0.3;               %m
+area = (diameter/2)^2*pi;    %m^2
+height = 0.10;              %m
 
 tubeHeightDrop = 1/150*length;
 
-syms velocity pressureDrop frictionLoss;
+syms velocity pressureDrop;
 % frictionLoss = length/diameter * (velocity^2)/(2*gravity) * frictionFactor;
 
 % frictionFactor = 1;
@@ -15,7 +15,7 @@ syms velocity pressureDrop frictionLoss;
 % velocity = sqrt(2*(gravity*height-pressureDrop/rho));
 
 frictionFactor = 0.05;
-eqn1 = [0 == -pressureDrop + rho*gravity*(length/diameter * (velocity^2)/(2*gravity) * frictionFactor) + rho*gravity*tubeHeightDrop, 0 == - velocity + sqrt(2*(gravity*height-pressureDrop/rho))];
+eqn1 = [pressureDrop == + rho*gravity*(length/diameter * (velocity^2)/(2*gravity) * frictionFactor) + rho*gravity*tubeHeightDrop, velocity == + sqrt(2*(gravity*height-pressureDrop/rho))];
 S = solve(eqn1, pressureDrop, velocity)
 
 

1b64d287a6c42a540c15ffe04aba40b5800f8da3
Cenzofy <35461425+Cenzofy@users.noreply.github.com>
2021-04-14 16:14:27 -0400

    Finalized Files
    Cleaned up comments and extraneous code

--- T_Simulation.m
+++ T_Simulation.m
@@ -1,5 +1,8 @@
 clear
 clc
+close
+
+%Model for computing the drainage of a tank with attached pipe and t-joint
 
 %Constants & Fluid Properties
 g = 9.81;             %m/s^2
@@ -10,7 +13,7 @@ minorLossFactor = 0.5+0.962;
 %Geometry
 d_1 = 0.00794;              %m
 d_2 = 0.01125;              %m
-L_1 = 0.42;                 %m
+L_1 = 0.42;                 %m %Key Adjustable Parameter
 L_2 = 0.02;                 %m
 
 area_1 = (d_1/2)^2*pi;      %m^2
@@ -21,7 +24,7 @@ tubeHeightDrop = 1/150*(L_1-0.02);
 roughness = 0.0025/1000;
 % relaRough = roughness/(diameter*1000);
 
-%While Loop Variable Init
+%Computation Initializations
 time = 1;
 notEmpty = true;
 heightArray = [];
@@ -29,9 +32,9 @@ reArray = [];
 frictionArray = [];
 
 
-syms velocity pressureDrop;
+syms velocity;
 
-b = roughness/14.8*(d_1/2)
+b = roughness/14.8*(d_1/2);
 
 frictionFactorGuess = 0.024;
 frictionFactorComp = 0;
@@ -41,19 +44,12 @@ while(notEmpty == true)
     iterations = 0;
     while((factorGuessDeviation > 0.002) & (iterations < 50))
         
-%         pDrop = (-15.12*velocity^2/32.25) -(g*(L_1)/150) + (L_1*velocity^2*frictionFactorGuess)/(2*d_1) + (L_2*velocity^2*frictionFactorGuess)/(32.24*d_2) + (minorLossFactor*(velocity^2)/2);
-        
-%         velEQN = [velocity == sqrt(2*g*height -pDrop)];
-%         S = solve(velEQN, velocity);
-
         velocity = sqrt((g*(300*height+L_1))/(150*(0.062 + L_1*frictionFactorGuess/d_1 + L_2*frictionFactorGuess/16.12/d_2 + minorLossFactor)));
-        %Re = double(S)*rho*d_1/viscosity;
         Re = velocity*rho*d_1/viscosity;
         a = 2.51/Re;
         frictionFactorComp = 1/(2*lambertw( log(10)/(2*a)*10^(b/(2*a)))/log(10) - (b/a))^2;
         factorGuessDeviation = abs(frictionFactorComp - frictionFactorGuess);
         frictionFactorGuess = frictionFactorComp;
-%         volumetricRate = double(S)*area_1;    
         volumetricRate = velocity*area_1;    
         
         iterations = iterations+1;
@@ -82,18 +78,20 @@ end
 fprintf("Total Time to Drain: %i", time)
 
 timeArray = linspace(1, time-1, time-1);
-subplot(3,1,1)
+
+figure(1)
+subplot(2,1,1)
 plot(timeArray, heightArray);
 title('Tank Height');
 ylabel('Height [m]');
 xlabel("Time (s)");
-subplot(3,1,2)
+subplot(2,1,2)
 plot(timeArray, reArray);
 title('Reynolds Number');
 ylabel('Re []');
 xlabel("Time (s)");
-subplot(3,1,3)
-plot(timeArray, frictionArray);
-title('Friction Factor');
-ylabel('Friction Factor []');
-xlabel("Time (s)");
+% subplot(3,1,3)
+% plot(timeArray, frictionArray);
+% title('Friction Factor');
+% ylabel('Friction Factor []');
+% xlabel("Time (s)");
--- Tube_Only_Simulation.m
+++ Tube_Only_Simulation.m
@@ -1,34 +1,43 @@
 clear
 clc
+close
 
+%Model for computing the drainage of a tank with attached pipe
+
+%Constants
 gravity = 9.81;             %m/s^2
 rho = 998;                  %kg/m^3
 diameter = 0.00794;         %m
-length = 0.2;               %m
+viscosity = 1.0016e-3;
+roughness = 0.0025/1000;
+% relaRough = roughness/(diameter*1000);
+
+%Geometry and Simulation Parameters
+length = 0.6;               %m
 area = (diameter/2)^2*pi;   %m^2
 height = 0.10;              %m
 volumeTotal = 0.32*0.26*0.1;
 tubeHeightDrop = 1/150*length;
+minorLossFactor = 0.5;
+
+%Computation Initializations
 time = 1;
 notEmpty = true;
 heightArray = [];
 reArray = [];
 frictionArray = [];
-viscosity = 1.0016e-3;
-roughness = 0.0025/1000;
-% relaRough = roughness/(diameter*1000);
-minorLossFactor = 0.5;
 
-syms velocity pressureDrop;
 
-b = roughness/14.8*(diameter/2)
+syms velocity;
+
+b = roughness/14.8*(diameter/2);
 
 frictionFactorGuess = 0.024;
 frictionFactorComp = 0;
 while(notEmpty == true)
     factorGuessDeviation = 10;
     iterations = 0;
-    while((factorGuessDeviation > 0.005) & (iterations < 50))
+    while((factorGuessDeviation > 0.002) & (iterations < 50))
         velocity = sqrt((150*gravity*height+gravity*length)/(75*(1+length*frictionFactorGuess/diameter+minorLossFactor)));
         Re = velocity*rho*diameter/viscosity;
         a = 2.51/Re;
@@ -37,7 +46,7 @@ while(notEmpty == true)
         frictionFactorGuess = frictionFactorComp;
         iterations = iterations+1;
 
-        fprintf("Re: %i\n", Re)
+        fprintf("Re: %f, It: %i\n", Re, iterations);
     end
     
     volumetricRate = velocity*area;    
@@ -62,18 +71,24 @@ end
 fprintf("Total Time to Drain: %i", time)
 
 timeArray = linspace(1, time-1, time-1);
-subplot(3,1,1)
+figure(1);
+subplot(2,1,1)
 plot(timeArray, heightArray);
-title('Tank Height');
+title('Tank Height over Time');
 ylabel('Height [m]');
 xlabel("Time (s)");
-subplot(3,1,2)
+% axis([0 205 0 0.1])
+legend("20cm Tube Length");
+subplot(2,1,2)
 plot(timeArray, reArray);
-title('Reynolds Number');
-ylabel('Re []');
+title('Reynolds Number over Time');
+ylabel('Re');
 xlabel("Time (s)");
-subplot(3,1,3)
-plot(timeArray, frictionArray);
-title('Friction Factor');
-ylabel('Friction Factor []');
-xlabel("Time (s)");
+% axis([0 205 3000 7500])
+legend("20cm Tube Length");
+
+% subplot(3,1,3)
+% plot(timeArray, frictionArray);
+% title('Friction Factor');
+% ylabel('Friction Factor []');
+% xlabel("Time (s)");
